06f83dd14fe4ad31d21df9d2a610354160634edd
= <jake_ps@comcast.net>
2015-08-17 20:47:50 -0700

    added validation for name in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :name
+
 
 end
